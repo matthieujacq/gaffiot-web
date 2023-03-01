7bbf2a171fcd9f80cf415036b0bc4ac89bb34ee0
mjacq <>
2023-03-01 16:34:53 +0100

    more colors + prev/next buttons

--- src/app/definition-card/definition-card.component.ts
+++ src/app/definition-card/definition-card.component.ts
@@ -17,10 +17,28 @@ export class DefinitionCardComponent {
   definition$ = this.wordId.pipe(
     map((id) => this.data[id]),
     tap((definition) => {
+      // Replace \tag{...} with <tag>...</tag>
       definition.french = definition.french.replace(
         /\\(\w+)\{([^}]+)}/g,
         '<$1>$2</$1>'
       );
+      // remove remaining \tag
+      definition.french = definition.french.replace(/\\(\w+)/g, '');
+      // enclose text between brackets in <i>...</i>
+      definition.french = definition.french.replace(
+        /\[([^\]]+)\]/g,
+        '<span class="comment">$1</span>'
+      );
+      // enclose text between </pp> and : or [
+      definition.french = definition.french.replace(
+        /<\/pp>([^:\[<]+)/g,
+        "</pp><span class='main-def'>$1</span>"
+      );
     })
+  );
+
+  prev$ = this.wordId.pipe(map((id) => (id ? this.data[id - 1] : null)));
+  next$ = this.wordId.pipe(
+    map((id) => (id < this.data.length - 1 ? this.data[id + 1] : null))
   );
 }
