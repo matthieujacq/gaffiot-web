83f11dfee36fc39ec2bd0d26addaab6c3d92fcf7
mjacq <>
2023-03-06 14:49:41 +0100

    pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +11,7 @@ import { SearchPageComponent } from './search-page/search-page.component';
 import { DefinitionCardComponent } from './definition-card/definition-card.component';
 import { SanitizeHTMLPipe } from './sanitize-html.pipe';
 import { LicencePageComponent } from './licence-page/licence-page.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,12 @@ import { LicencePageComponent } from './licence-page/licence-page.component';
     SanitizeHTMLPipe,
     LicencePageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [],
   bootstrap: [AppComponent],
 })
